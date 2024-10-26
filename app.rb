1db205ad09ec5074c5cc3526f34b2962ae6eab68
Max Temkin <Max.Temkin@Gmail.com>
2024-10-26 02:47:36 +0000

    setting up

--- app.rb
+++ app.rb
@@ -2,8 +2,29 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:square)
+end
+
+#Square
+
+get("/square/new") do
+  erb(:square)
+end
+
+get("/square/results") do
+  @number = params.fetch("number").to_i
+  @square_result = @number**2
+  erb(:squareresults)
+end
+
+#Square Root
+
+get("/square_root/new") do
+  erb(:squareroot)
+end
+
+get("/square_root/results") do
+  @number = params.fetch("number").to_i
+  @result = @number**0.5
+  erb(:squarerootresults)
 end
